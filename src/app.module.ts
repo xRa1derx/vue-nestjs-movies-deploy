4508618b06e3f51eff620344a7bf867b9e75ad06
xRa1derx <raider742617@gmail.com>
2023-06-22 19:42:56 +0300

    first comment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FilmsModule } from './films/films.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,11 +8,14 @@ import { FilesModule } from './files/files.module';
 import { MulterModule } from '@nestjs/platform-express';
 import { ServeStaticModule } from '@nestjs/serve-static/dist/serve-static.module';
 import { join } from 'path';
+import { MongooseConfigService } from './config/MongooseConfigService';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     FilmsModule,
-    MongooseModule.forRoot(`mongodb+srv://raider742617:varvara24082015@cluster0.kmqltpf.mongodb.net/?retryWrites=true&w=majority`),
+    MongooseModule.forRootAsync({ imports: [ConfigModule], useClass: MongooseConfigService }),
+    ConfigModule.forRoot({ load: [configuration] }),
     UsersModule,
     AuthModule,
     FilesModule,
@@ -24,7 +26,5 @@ import { join } from 'path';
       rootPath: join(__dirname, '..', 'public'),
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+@Module({
+  imports: [
+    MongooseModule
+  ]
+})
+
+export class AppModule { }
